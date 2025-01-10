7bdec160964e277443c06dc418abcc8e3f335f34
Alina329-web <alinabodnarchuk1985r@gmail.com>
2025-01-10 20:39:46 +0200

    hw1_3

---
+++ src/FriendListItem.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import styles from "./FriendListItem.module.css";
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <div className={styles.friendItem}>
+      <img
+        src={avatar}
+        alt={`${name} avatar`}
+        width="48"
+        className={styles.avatar}
+      />
+      <p className={styles.name}>{name}</p>
+      <p
+        className={`${styles.status} ${
+          isOnline ? styles.online : styles.offline
+        }`}
+      >
+        {isOnline ? 'Online' : 'Offline'}
+      </p>
+    </div>
+  );
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
+export default FriendListItem;
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import styles from './TransactionHistory.module.css';
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={styles.transactionTable}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody className={styles.transactoonTbody}>
+        {items.map(({ type, amount, currency, id }) => (
+          <tr key={id} className={styles.transactoonTr}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.number.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+export default TransactionHistory;
